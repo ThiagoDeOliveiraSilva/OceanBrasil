8bee6b4ec9f00e699e08ddb38fd66d5e4d79eefc
ThiagoDeOliveiraSilva <thiagoo.silva9@hotmail.com>
2023-02-16 00:01:57 +0000

    Meu Blog

--- app.py
+++ app.py
@@ -1,24 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash, g, abort
+import sqlite3
 
 app = Flask (__name__)
 
-#POST MOCK
-posts = [
-    {
-        "titulo": "Post 1",
-        "texto": "Meu primeiro Post"
-    },
-    {
-        "titulo": "Post 2",
-        "texto": "olha eu aqui de novo"
-    },
-    {
-        "titulo": "Post 3",
-        "texto": "Novo Post"
-    }
-]
+SECRET_KEY = "pudim"
+DATABASE = "blog.bd"
+app.config.from_object(__name__)
 
+#USER MOCKS
+USERNAME = "admin"
+PASSWORD = "admin"
+
+def conectar_bd():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def pre_requisicao():
+    g.bd = conectar_bd()
+
+@app.teardown_request
+def encerrar_requisicao(exception):
+    g.bd.close()
 
 @app.route('/')
 def exibir_entradas():
-    return render_template('exibir_entradas.html', entradas=posts)
+    sql = 'SELECT titulo, texto FROM entradas ORDER BY id DESC'
+    cursor = g.bd.execute(sql)
+    entradas = [{"titulo": titulo, "texto": texto} for titulo, texto in cursor.fetchall()]
+    return render_template('exibir_entradas.html', entradas=entradas)
+
+@app.route('/inserir', methods=["post"])
+def inserir_entradas():
+    if not session['logado']:
+        abort(401)
+    sql = "INSERT INTO entradas(titulo, texto) VALUES (?, ?)"
+    g.bd.execute(sql, [request.form['titulo'], request.form['texto']])
+    g.bd.commit()
+    flash('Novo post criado com sucesso!')
+    return redirect(url_for('exibir_entradas'))
+
+@app.route('/login', methods=["get","post"])
+def login():
+    erro = ''
+    if request.method == "POST":
+        if request.form['username'] == USERNAME and request.form['password'] == PASSWORD:
+            session['logado'] = True
+            flash('login efetuado com sucesso')
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou Senha inválidos"
+    return render_template('login.html', erro=erro)
+
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    flash('Logout efetuado com suecsso')
+    return redirect(url_for('exibir_entradas'))
